d7c3061ec9f39b4a3c065b798125f2a7c10a06df
anthony-broderick <atbrod20@gmail.com>
2025-10-30 12:24:59 -0400

    Created globals.py. Updated netlist_parser.py to use the global variables and clears them before every new netlist.

---
+++ globals.py
@@ -0,0 +1,15 @@
+# given core data
+primary_inputs = []
+primary_outputs = []
+gates = []
+
+# wire values
+
+# faults
+
+# reset for new netlist
+def reset_globals():
+    global primary_inputs, primary_outputs, gates
+    primary_inputs.clear()
+    primary_outputs.clear()
+    gates.clear()
--- netlist_parser.py
+++ netlist_parser.py
@@ -1,4 +1,5 @@
 import os
+import globals
 
 def read_netlist(filepath: str):
     # validate file
@@ -9,10 +10,9 @@ def read_netlist(filepath: str):
     if not filepath.endswith('.ckt'):
         print(f"Error: Invalid file type. Must be a .ckt file.")
         return None
-
-    primary_inputs = []
-    primary_outputs = []
-    gates = []
+    
+    # clear any previous netlist data
+    globals.reset_globals()
 
     # read and strip lines
     with open(filepath, "r") as file:
@@ -26,25 +26,25 @@ def read_netlist(filepath: str):
             # primary inputs
             if "$... primary input" in line or "$ ... primary input" in line:
                 wire_name = tokens[0]
-                primary_inputs.append(wire_name)
+                globals.primary_inputs.append(wire_name)
 
             # primary outputs
             elif "$... primary output" in line or "$ ... primary output" in line:
                 wire_name = tokens[0]
-                primary_outputs.append(wire_name)
+                globals.primary_outputs.append(wire_name)
             
             # gates
             else:
                 if len(tokens) == 4:
                     output, gate_type, input1, input2 = tokens
-                    gates.append({
+                    globals.gates.append({
                         "output": output,
                         "gate_type": gate_type.lower(),
                         "inputs": [input1, input2]
                     })
                 elif len(tokens) == 3:
                     output, gate_type, input1 = tokens
-                    gates.append({
+                    globals.gates.append({
                         "output": output,
                         "gate_type": gate_type.lower(),
                         "inputs": [input1]
@@ -53,13 +53,11 @@ def read_netlist(filepath: str):
                     print(f"Warning: Unrecognized line format: '{line}'")
 
     # display for testing
-    print(f"Primary Inputs: {primary_inputs}")
-    print(f"Primary Outputs: {primary_outputs}")
+    print(f"Primary Inputs: {globals.primary_inputs}")
+    print(f"Primary Outputs: {globals.primary_outputs}")
     print("Gates:")
-    for gate in gates:
+    for gate in globals.gates:
         print(f"  {gate['output']} {gate['gate_type']} {gate['inputs']}")
-    
-    return primary_inputs, primary_outputs, gates
 
 # test runner
 if __name__ == "__main__":
